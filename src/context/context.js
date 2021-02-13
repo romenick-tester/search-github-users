b829225c26c4f0bd5fcbcd61146da316a9cacab9
Romenick Tester <romenick.tester@gmail.com>
2021-02-13 12:48:50 +0000

    Context Provider

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import mockUser from "./mockData.js/mockUser";
 import mockRepos from "./mockData.js/mockRepos";
 import mockFollowers from "./mockData.js/mockFollowers";
 import axios from "axios";
 
 const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const stateValues = {
+    msg: "Hello"
+}
+
+const GithubProvider = ({ children }) => {
+    return (
+        <GithubContext.Provider value={{...stateValues}}>
+            {children}
+        </GithubContext.Provider>
+    )
+}
+
+export { GithubProvider, GithubContext };
