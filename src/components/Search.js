81719e1e2dd222d124adbc00810c113cc602ce57
Romenick Tester <romenick.tester@gmail.com>
2021-02-15 12:26:23 +0000

    Search Component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,8 +2,39 @@ import React from "react";
 import styled from "styled-components";
 import { MdSearch } from "react-icons/md";
 import { GithubContext } from "../context/context";
+
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState("");
+
+  //const data = React.useContext(GithubContext);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(user) {
+      console.log(user);
+    } else {
+      console.log("your query is empty!");
+    }
+  };
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch/>
+            <input 
+              type="text" 
+              placeholder="enter github user" 
+              value={user} onChange={(e) => setUser(e.target.value)} 
+            />
+            <button type="submit">search</button>
+          </div>
+        </form>
+        <h3>request: 60 / 60</h3>
+      </Wrapper>
+    </section>
+  )
 };
 
 const Wrapper = styled.div`
